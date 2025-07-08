38e8715f1afa3b99012c10a693fe99f8766fda77
Sandeepsitm91 <sandeepsitm91@gmail.com>
2025-07-08 13:03:18 +0530

    rg1-1

--- main.tf
+++ main.tf
@@ -2,6 +2,9 @@ resource "azurerm_resource_group" "rg1" {
     name = "rg1"
     location = "West Us2"
 }
-
+resource "azurerm_resource_group" "rg1-1" {
+    name = "rg1-1"
+    location = "West Us2"
+}
 
 
