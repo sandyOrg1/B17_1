b2bfdfa22dc162cd4c5de2d8b8e47148913f8e8e
sandeepsitm91 <sandeepsitm91@gmail.com>
2025-07-08 13:10:52 +0530

    Feature/sandy1 (#8)
    * rg1-1-1
    
    * 2 commit rg1-1-1-1
    
    * 3 rg rmv
    
    * 3RG RMV again
    
    * rg1-1
    
    ---------
    
    Co-authored-by: rakeshsitm91 <rakeshsitm91@gmail.com>

--- main.tf
+++ main.tf
@@ -2,7 +2,10 @@ resource "azurerm_resource_group" "rg1" {
     name = "rg1"
     location = "West Us2"
 }
-
+resource "azurerm_resource_group" "rg1-1" {
+    name = "rg1-1"
+    location = "West Us2"
+}
 
 
 
